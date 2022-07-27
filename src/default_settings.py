75e21ab18861ec61d9cedf265d6ceb12be9170ff
Vincent <panouilleres.vincent@yahoo.fr>
2022-07-27 17:56:23 +0200

    Ajout du contenu de default_settings.py

---
+++ src/default_settings.py
@@ -0,0 +1,10 @@
+#################################################################################################
+#                Ce fichier contient le paramétrage du bot discord.                             # 
+#  Ce fichier doit être renommé en settings.py dans le même répertoire que l'éxécutable du bot  #
+# /!\   Le fichier settings.py doit toujours être ignoré par le gestionnaire de version     /!\ #
+#################################################################################################
+#
+# Cette varialbe contient le toke délivré par le portail développeur discord
+my_discord_token='token_ici'
+# Cette variable contient le path vers la bbase de donnée sqlite
+db_path='/path/vers/ma/db.sqlite'
